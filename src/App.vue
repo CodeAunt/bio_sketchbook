e13d32ee0a6eeb8d55ecd6d8aa7fc2a8dc5a9863
zhangchao <373930051@qq.com>
2021-12-29 14:33:31 +0800

    update canvas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <div class="absolute z-50 right-9 top-8">
+    <!-- <div class="absolute z-50 right-9 top-8">
       <img src="https://raw.githubusercontent.com/chaochaooo/Bio_Sketchbook/main/public/assets/svg/collector.svg">
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
